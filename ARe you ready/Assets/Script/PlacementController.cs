c10bc474e9e5cb7464cd868a1eac657685275f85
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-10-04 23:31:21 -0700

    Scale wip

--- ARe you ready/Assets/Script/PlacementController.cs
+++ ARe you ready/Assets/Script/PlacementController.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -132,4 +132,3 @@ public class PlacementController : MonoBehaviour
         }
     }
 }
-*/
