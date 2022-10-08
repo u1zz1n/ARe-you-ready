be8b055f00c073430dc93e2cc61c7512059db526
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-10-08 15:18:03 -0700

    place the ball before it roll

--- ARe you ready/Assets/Script/PlacementAndDragging.cs
+++ ARe you ready/Assets/Script/PlacementAndDragging.cs
@@ -9,6 +9,9 @@ public class PlacementAndDragging : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public static bool placeBowling;
+    private bool ballspawn;
+
     public ARRaycastManager arRaycastManager;
     public ARSessionOrigin aRSessionOrigin;
 
@@ -17,6 +20,9 @@ public class PlacementAndDragging : MonoBehaviour
 
     [SerializeField]
     private GameObject placedPrefab;
+
+    [SerializeField]
+    private GameObject bowingBall;
 
     private PlacementObject[] placedObjects;
 
@@ -58,6 +64,8 @@ public class PlacementAndDragging : MonoBehaviour
         }
     }
 
+    
+
     void Awake()
     {
     }
@@ -78,14 +86,13 @@ public class PlacementAndDragging : MonoBehaviour
 
     void Start()
     {
+        placeBowling = false;
+        ballspawn = false;
         scaleSlider.onValueChanged.AddListener(ScaleChanged);
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlaceBowlingBall()
     {
-        hihi.text = "scale is " + scaleSlider.value.ToString();
-
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -143,9 +150,10 @@ public class PlacementAndDragging : MonoBehaviour
             {
                 Pose hitPose = hits[0].pose;
 
-                if (lastSelectedObject == null)
+                if (lastSelectedObject == null && !ballspawn)
                 {
-                    lastSelectedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation).GetComponent<PlacementObject>();
+                    ballspawn = true;
+                    lastSelectedObject = Instantiate(bowingBall, hitPose.position, hitPose.rotation).GetComponent<PlacementObject>();
                 }
                 else
                 {
@@ -157,6 +165,91 @@ public class PlacementAndDragging : MonoBehaviour
                 }
             }
         }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(placeBowling)
+        {
+            hihi.text = "CLICK";
+            PlaceBowlingBall();            
+        }
+        else{
+            hihi.text = "scale is " + scaleSlider.value.ToString();
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                {
+                    return;
+                }
+
+                touchPosition = touch.position;
+
+                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Ray ray = arCamera.ScreenPointToRay(touch.position);
+                    RaycastHit hitObject;
+
+                    if (Physics.Raycast(ray, out hitObject))
+                    {
+                        lastSelectedObject = hitObject.transform.GetComponent<PlacementObject>();
+
+                        if (lastSelectedObject != null)
+                        {
+                            PlacementObject[] allOtherObjects = FindObjectsOfType<PlacementObject>();
+
+                            foreach (PlacementObject placementObject in allOtherObjects)
+                            {
+                                MeshRenderer meshRenderer = placementObject.GetComponent<MeshRenderer>();
+
+                                if (placementObject != lastSelectedObject)
+                                {
+                                    placementObject.Selected = false;
+                                    meshRenderer.material.color = inactiveColor;
+                                }
+                                else
+                                {
+                                    placementObject.Selected = true;
+                                    meshRenderer.material.color = activeColor;
+                                }
+
+                                if (displayOverlay)
+                                    placementObject.ToggleOverlay();
+                            }
+                        }
+                    }
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    lastSelectedObject.Selected = false;
+                }
+
+                if (arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hits[0].pose;
+
+                    if (lastSelectedObject == null)
+                    {
+                        lastSelectedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation).GetComponent<PlacementObject>();
+                    }
+                    else
+                    {
+                        if (lastSelectedObject.Selected)
+                        {
+                            lastSelectedObject.transform.position = hitPose.position;
+                            lastSelectedObject.transform.rotation = hitPose.rotation;
+                        }
+                    }
+                }
+            }
+        }
+        
     }
 
     private void ScaleChanged(float newValue)
