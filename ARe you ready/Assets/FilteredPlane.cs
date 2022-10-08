a88a42dedcd9964a0eb205e5f2ca433119cbcd4e
Hagyeong Kim <97077255+digipenHagyeong@users.noreply.github.com>
2022-10-08 15:19:53 -0700

    check plane

---
+++ ARe you ready/Assets/FilteredPlane.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class FilteredPlane : MonoBehaviour
+{
+    [SerializeField] private Vector2 dismenstionsForBigPlane;
+
+    public event Action OnVerticalPlaneFound;
+    public event Action OnHorizontalPlaneFound;
+    public event Action OnBigPlaneFound;
+
+    private ARPlaneManager arPlaneManager;
+    private List<ARPlane> arPlanes;
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        if (args.added != null && args.added.Count > 0)
+        {
+            arPlanes.AddRange(args.added);
+        }
+        
+        foreach (ARPlane plane in arPlanes.Where(plane => plane.extents.x * plane.extents.y >= 0.1f))
+        {
+            if(plane.alignment.IsVertical())
+            {
+                OnVerticalPlaneFound.Invoke();
+            }
+           
+            if(plane.alignment.IsHorizontal())
+            {
+                OnHorizontalPlaneFound.Invoke();
+            }   
+
+            if(plane.extents.x * plane.extents.y >= dismenstionsForBigPlane.x * dismenstionsForBigPlane.y)
+            {
+                OnBigPlaneFound.Invoke();
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        arPlanes = new List<ARPlane>();
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        arPlaneManager.planesChanged += OnPlanesChanged;
+    }
+
+    private void OnDisable()
+    {
+        arPlaneManager.planesChanged -= OnPlanesChanged;
+    }
+
+
+}
