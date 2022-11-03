c833e7fd06a4f2fd3d003dcbde7d250212dbe236
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-11-03 02:28:20 -0700

    Add MainMene that allows player to select to go to tech demo or simulation demo

---
+++ ARe you ready/Assets/ManageScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ManageScene : MonoBehaviour
+{
+    public void TechDemo()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void BowlingDemo()
+    {
+        SceneManager.LoadScene("RollBall");
+    }
+}
