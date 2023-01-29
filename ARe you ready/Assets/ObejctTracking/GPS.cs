7e5540195aa99ae1adb2c9d94a6e8141f827d9e6
Hagyeong Kim <97077255+digipenHagyeong@users.noreply.github.com>
2023-01-29 03:22:48 -0800

    checking current location

--- ARe you ready/Assets/ObejctTracking/GPS.cs
+++ ARe you ready/Assets/ObejctTracking/GPS.cs
@@ -67,6 +67,10 @@ public class GPS : MonoBehaviour
     public Text[] data = new Text[4];
     //public float maxTime = 100.0f;
 
+    float latitudeCur;
+    float longitudeCur;
+    float altitudeCur;
+
     void Start()
     {
         StartCoroutine(GPS_manager());
@@ -111,15 +115,18 @@ public class GPS : MonoBehaviour
             yield break;
         }
 
-        data[0].text = "Latitude : " + Input.location.lastData.latitude.ToString();
-        data[1].text = "Longitude : " + Input.location.lastData.longitude.ToString();
-        data[2].text = "Altitude : " + Input.location.lastData.altitude.ToString();
+        //latitudeCur = Input.location.lastData.latitude;
+        //longitudeCur = Input.location.lastData.longitude;
+        //altitudeCur = Input.location.lastData.altitude;
+
+        data[0].text = "Latitude : " + latitudeCur.ToString();
+        data[1].text = "Longitude : " + longitudeCur.ToString();
+        data[2].text = "Altitude : " + altitudeCur.ToString();
         data[3].text = "Done";
 
         Input.location.Stop();
         
         yield break;
-
     }
     void Update()
     {
@@ -133,5 +140,56 @@ public class GPS : MonoBehaviour
         //home = 47.681565 -122.127662
         //home = 47.681500 -122.131734
 
+        float starL = 47.681042f;
+        float starLo = -122.2125707f;
+
+        latitudeCur = 47.680373f;
+        longitudeCur = -122.125507f;
+
+        if (CalculateLocationOffset(starL, starLo) < 100.0f) //nono
+        {
+            data[0].text = CalculateLocationOffset(starL, starLo).ToString(); //0.03002862
+        }
+        else
+        {
+            data[0].text = "NOnono";
+        }
+
+        latitudeCur = 47.681056f;
+        longitudeCur = -122.125705f;
+
+        if (CalculateLocationOffset(starL, starLo) < 100.0f) //yesyes
+        {
+            data[1].text = CalculateLocationOffset(starL, starLo).ToString(); //0.0006848066
+        }
+        else
+        {
+            data[1].text = "NOnono";
+        }
+
+        latitudeCur = 47.681236f;
+        longitudeCur = -122.125888f;
+
+        if (CalculateLocationOffset(starL, starLo) < 100.0f) //yesyes
+        {
+            data[2].text = CalculateLocationOffset(starL, starLo).ToString(); //0.00871902
+        }
+        else
+        {
+            data[2].text = "NOnono";
+        }
+    }
+
+    private float CalculateLocationOffset(float latitudeB, float longitudeB)
+    {
+        float firstSinSqure = Mathf.Sin((Mathf.PI * (latitudeB - latitudeCur)/ 360) * Mathf.Sin((Mathf.PI * (latitudeB - latitudeCur)) / 360));
+        float firstCosSqure = Mathf.Cos((Mathf.PI * longitudeB) / 180);
+        float secondCosSqure = Mathf.Cos((Mathf.PI * longitudeCur) / 180);
+        float secondSinSqure = Mathf.Sin((Mathf.PI * (longitudeB - longitudeCur) / 360) * Mathf.Sin((Mathf.PI * (longitudeB - longitudeCur)) / 360));
+
+        float inDistanceSqure = Mathf.Sqrt(firstSinSqure * firstCosSqure * secondCosSqure * secondSinSqure);
+        float distance = 127420000000 * Mathf.Asin(inDistanceSqure);
+
+        return distance;
     }
 }
