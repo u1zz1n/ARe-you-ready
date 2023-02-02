7321b637dc763bcb835ddba44148628ae1c62d51
Uijin-Lee <digipen.uijin.lee@gmail.com>
2023-02-01 23:32:22 -0800

    tracked multiple images and instantiate specific game object per images.

--- ARe you ready/Assets/Script/ImageTracking/TrackedImageInfoMultipleManager.cs
+++ ARe you ready/Assets/Script/ImageTracking/TrackedImageInfoMultipleManager.cs
@@ -8,23 +8,39 @@ using UnityEngine.XR.ARFoundation;
 public class TrackedImageInfoMultipleManager : MonoBehaviour
 {
     [SerializeField]
+    private Text DebugLog;
+
+    [SerializeField]
+    private Text DebugLog2;
+
+    [SerializeField]
+    private Text imageTrackedText;
+
+    [SerializeField]
     private GameObject[] arObjectsToPlace;
 
     [SerializeField]
-    private Vector3 scaleFactor = new Vector3(0.1f, 0.1f, 0.1f);
+    private Vector3 scaleFactor = new Vector3(0.05f, 0.05f, 0.05f);
 
+    [SerializeField]
     private ARTrackedImageManager m_TrackedImageManager;
 
     private Dictionary<string, GameObject> arObjects = new Dictionary<string, GameObject>();
 
     private void Awake() {
-        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+       //m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+
+        if(arObjectsToPlace == null)
+        {
+            DebugLog.text = "empty";
+        }
 
         foreach(GameObject arObject in arObjectsToPlace)
         {
             GameObject newARObject = Instantiate(arObject, Vector3.zero, Quaternion.identity);
             newARObject.name = arObject.name;
             arObjects.Add(arObject.name, newARObject);
+            DebugLog2.text += arObjects[arObject.name].name;
         }
     }
 
@@ -38,17 +54,17 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
 
     void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        foreach(ARTrackedImage trackedImage in eventArgs.added)
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
             UpdateARImage(trackedImage);
         }
 
-        foreach(ARTrackedImage trackedImage in eventArgs.updated)
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
             UpdateARImage(trackedImage);
         }
 
-        foreach(ARTrackedImage trackedImage in eventArgs.removed)
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
         {
             arObjects[trackedImage.name].SetActive(false);
         }
@@ -56,18 +72,22 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
 
     private void UpdateARImage(ARTrackedImage trackedImage)
     {
+        imageTrackedText.text = trackedImage.referenceImage.name;
         AssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position);
+        Debug.Log($"trackedImage.referenceImage.name: {trackedImage.referenceImage.name}");
     }
 
     void AssignGameObject(string name, Vector3 newPosition)
     {
         if(arObjectsToPlace != null)
         {
-            arObjects[name].SetActive(true);
-            arObjects[name].transform.position = newPosition;
-            arObjects[name].transform.localScale = scaleFactor;
+            GameObject goARObject = arObjects[name];
+            goARObject.SetActive(true);
+            goARObject.transform.position = newPosition;
+            goARObject.transform.localScale = scaleFactor;
             foreach(GameObject go in arObjects.Values)
             {
+                Debug.Log($"Go in arObjects.Values: {go.name}");
                 if(go.name != name) {
                     go.SetActive(false);      
                 }
